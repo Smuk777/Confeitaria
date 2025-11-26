379d854a978b368627c64bec1ac91a920b6598a4
Smuk777 <cauaacarvalho@gmail.com>
2025-11-26 20:14:34 -0300

    criando item com js

---
+++ cardapio.js
@@ -0,0 +1,30 @@
+function criarItemCardapio(titulo){
+
+    const divItemCardapio = document.createElement('div')
+    divItemCardapio.className = 'item-cardapio'
+
+    const h3Titulo = document.createElement('h3')
+    h3Titulo.textContent = titulo 
+
+    const pDescricao = document.createElement('p')
+    pDescricao.textContent = descricao
+    pDescricao.className = 'descricao'
+
+    const img = document.createElement('img')
+    img.src = foto 
+    img.className = 'img-item'
+
+    const divC = document.getElementById('cardapio')
+
+    divItemCardapio.appendChild(h3Titulo)
+    divItemCardapio.appendChild(pDescricao)
+    divItemCardapio.appendChild(img)
+
+    divC.appendChild(divItemCardapio)
+}
+
+criarItemCardapio (
+    'Bolo de Chocolate',
+    'Um clássico',
+    'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2020/05/bolo_chocolate_leite.jpg'
+)
